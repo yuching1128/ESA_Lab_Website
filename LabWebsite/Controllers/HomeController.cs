12646b44f27988011418ebc70d734b380e80e4e2
wanda_jih <wanda_jih@gss.com.tw>
2018-11-13 17:06:59 +0800

    no message

---
+++ LabWebsite/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "實驗室簡介";
+
+            return View();
+        }
+
+        public ActionResult Professor()
+        {
+            ViewBag.Message = "指導教授";
+
+            return View();
+        }
+
+        public ActionResult Member()
+        {
+            ViewBag.Message = "實驗室成員";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "聯絡我";
+
+            return View();
+        }
+    }
+}
+
---
+++ LabWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LabWebsite.MvcApplication" Language="C#" %>
