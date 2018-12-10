c13663da067e01252cd60d812be9cb0d9f07dbf4
wanda_jih <wenney8686@gmail.com>
2018-12-10 19:44:37 +0800

    finally

---
+++ 專案/LabWebsite/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "實驗室簡介";
+
+            return View();
+        }
+
+        public ActionResult Professor()
+        {
+            ViewBag.Message = "指導教授";
+
+            return View();
+        }
+        public ActionResult Publication()
+        {
+            ViewBag.Message = "著作";
+
+            return View();
+        }
+        public ActionResult Research()
+        {
+            ViewBag.Message = "研究計畫";
+
+            return View();
+        }
+        public ActionResult Patent()
+        {
+            ViewBag.Message = "專利";
+
+            return View();
+        }
+        public ActionResult Honors()
+        {
+            ViewBag.Message = "獲獎殊榮";
+
+            return View();
+        }
+
+        public ActionResult Member()
+        {
+            ViewBag.Message = "實驗室成員";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "聯絡我";
+
+            return View();
+        }
+    }
+}
+
